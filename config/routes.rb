d860597984e28804a516791d455cb0d899f40d26
Rich <rpavesi84@gmail.com>
2020-03-02 13:43:13 -0500

    adds routes for testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'demo#index'
+
   get 'demo/index'
+  get 'demo/hello'
+  # default routes no longer in favor
+  # get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
