13d4de4d0edb6279947cc63fc1ee7a7a8e615be9
Rich <rpavesi84@gmail.com>
2020-03-02 14:43:34 -0500

    test external link redirect

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'demo/index'
   get 'demo/hello'
   get 'demo/other_hello'
+  get 'demo/lynda'
   # default routes no longer in favor
   # get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
