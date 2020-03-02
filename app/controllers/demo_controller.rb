dd796fdd7bce2c9455b93e704045ad430f0aa001
Rich <rpavesi84@gmail.com>
2020-03-02 14:40:23 -0500

    tests rails app redirect action

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -10,4 +10,8 @@ class DemoController < ApplicationController
   def hello
     render('index')
   end
+
+  def other_hello
+    redirect_to(action: 'hello')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get 'demo/index'
   get 'demo/hello'
+  get 'demo/other_hello'
   # default routes no longer in favor
   # get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
